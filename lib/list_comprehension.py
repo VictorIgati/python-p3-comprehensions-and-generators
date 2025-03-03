334568392a1b1ae6bf0b1674a0dc13c3bb547848
VictorIgati <victor.igati@student.moringaschool.com>
2025-03-03 16:37:36 +0300

    done this lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -2,4 +2,4 @@ def return_evens(num_list):
     return [n for n in num_list if n % 2 == 0]
 
 def make_exclamation(sentence_list):
-    return [sentence + "!" for sentence in sentence_list]   
+    return [sentence + "!" for sentence in sentence_list] 
